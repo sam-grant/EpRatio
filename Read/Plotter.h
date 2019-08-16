b27233b154bafd667898d56531959b7487fe7287
Samuel Grant <samuelgrant@x.dhcp.fnal.gov>
2019-08-16 17:55:33 -0500

    Fixed the pull plots

--- Read/Plotter.h
+++ Read/Plotter.h
@@ -62,7 +62,7 @@ public:
 
   bool FiducialXtal(const double x, const double y);
   bool FiducialMain(const double x, const double y);
-  bool FiducialHacky(const int xtal);
+  bool FiducialEdge(const int xtal);
 
   //could add any number of readers here:
   clusterTrackerTrackReader *ctt;
@@ -370,11 +370,11 @@ bool Plotter::FiducialMain(const double x, const double y) {
   // if(fidy && fidx) fid = true;
 }
 
-bool Plotter::FiducialHacky(const int xtal) {
+bool Plotter::FiducialEdge(const int xtal) {
   bool fid = true;
-  int i[26] = {0,1,2,3,4,5,6,7,8,9,17,18,26,27,35,36,44,45,46,47,48,49,50,51,52,53};
-  for(int loop(0); loop<26; loop++) {
-    if(xtal == i[loop]) {
+  int edge[26] = {0,1,2,3,4,5,6,7,8,9,17,18,26,27,35,36,44,45,46,47,48,49,50,51,52,53};
+  for(int i(0); i<26; i++) {
+    if(xtal == edge[i]) {
       fid=false;
       break;
     }
