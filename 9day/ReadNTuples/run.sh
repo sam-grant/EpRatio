3704691262fc9ed988402e9146c9623c0c4062f6
GM2 Daq User <daq@gm2liv.fnal.gov>
2019-11-22 13:48:42 -0600

    Made full calo plotter

--- 9day/ReadNTuples/run.sh
+++ 9day/ReadNTuples/run.sh
@@ -3,10 +3,11 @@ for file in `ls /mnt/strawRaid01/omegaEU/9day_5040A/gm2skim_ana_*.root`; do
     echo $file
 #    ./PlotterEpXtalFid_9day $file
 #    ./PlotterTimeXtal_9day $file
-    ./PlotterTimeXtal_9day $file
-
+#    ./PlotterTimeXtalCorr_9day $file
+    ./PlotterTimeCalo_9day $file
 done
 
 #hadd ../ROOT/RawEpMeansFid_9day.root plots*.root
 #hadd ../ROOT/RawTimeXtalFid_9day.root plots*.root
-hadd ../ROOT/RawTimeXtalFidCorr_9day.root plots*.root
+#hadd ../ROOT/RawTimeXtalFidCorr_9day.root plots*.root
+hadd ../ROOT/RawTimeCalo_9day.root plots*.root
